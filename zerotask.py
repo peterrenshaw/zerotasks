ae696f7aa5be93dde4e78faec1b3e8c99d24f6f6
Peter Renshaw <peterrenshaw@seldomlogical.com>
2016-11-10 15:31:57 +1100

    initial commit

---
+++ zerotask.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# ~*~ encoding: utf-8 ~*~
+
+
+#======
+# name: tsk.py
+# date: 2016NOV07
+# prog: pr
+# desc: zero task
+#======
+
+
+import os
+import json
+import time
+import glob
+import datetime
+import argparse
+
+
+
+def main():
+    """main entry point for cli"""
+    pass
+
+
+# main entry point for cli
+if __name__  == "__main__":
+    main()
+
+
+# vim: ff=unix:ts=4:sw=4:tw=78:noai:expandtab

383feed8b99a1400aa8850f4ee24850b9d0d80dc
Peter Renshaw <peterrenshaw@seldomlogical.com>
2016-11-13 09:45:15 +1100

    2016NOV13 machine.py updated

---
+++ machine.py
@@ -0,0 +1,39 @@
+# ~*~ encoding: utf-8 ~*~
+
+
+#=======
+#  _____                      ______           __       
+# /__  /  ___  _________     /_  __/___ ______/ /_______
+#   / /  / _ \/ ___/ __ \     / / / __ `/ ___/ //_/ ___/
+#  / /__/  __/ /  / /_/ /    / / / /_/ (__  ) ,< (__  ) 
+# /____/\___/_/   \____/    /_/  \__,_/____/_/|_/____/  
+#
+# name: machine.py
+# date: 2016NOV10
+# prog: pr
+# desc: machine dependent Zero Tasks: read docs/ABOUT.txt
+#======
+
+
+#------
+# DIR structure is as follows
+#
+#     /HOME/REL_PATH/APP_DIR
+#
+HOME = "/path/to/user"
+REL_PATH = "relative/path"
+APP_DIR = "zerotasks"
+#
+#------
+
+def main():
+    """main entry point for cli"""
+    pass
+
+
+# main entry point for cli
+if __name__  == "__main__":
+    main()
+
+
+# vim: ff=unix:ts=4:sw=4:tw=78:noai:expandtab

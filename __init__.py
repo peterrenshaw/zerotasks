2acc2373f00cf8888cdd27398db771fe76398531
Peter Renshaw <peterrenshaw@seldomlogical.com>
2016-11-12 16:35:47 +1100

    2016NOV12 building setup

---
+++ __init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# ~*~ encoding: utf-8 ~*~
+
+
+__version__ = '0.0.1'
+__author__ = 'Peter Renshaw'
+__license__ = 'GNU GPL 3.0'
+__email__ = 'peterenshaw@seldomlogical.com'
+__url__ = 'https://github.com/peterrenshaw/zerotasks'
+__description__ = """This is a TODO system that I want to display the latest things I'm doing on both console and webpage AND/OR service online. In the example above, the TODO list is integrated into bash and the console."""
+
+
+__all__ = ['docs']
+
+
+# vim: ff=unix:ts=4:sw=4:tw=78:noai:expandtab
---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# ~*~ encoding: utf-8 ~*~
+
+
+#=======
+#  _____                      ______           __       
+# /__  /  ___  _________     /_  __/___ ______/ /_______
+#   / /  / _ \/ ___/ __ \     / / / __ `/ ___/ //_/ ___/
+#  / /__/  __/ /  / /_/ /    / / / /_/ (__  ) ,< (__  ) 
+# /____/\___/_/   \____/    /_/  \__,_/____/_/|_/____/  
+#
+# name: zerotask.py
+# date: 2016NOV07
+# prog: pr
+# desc: Zero Tasks: read docs/ABOUT.txt
+#======
+
+
+
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+
+from zerotasks import __url__
+from zerotasks import __email__
+from zerotasks import __author__
+from zerotasks import __license__
+from zerotasks import __version__
+from zerotasks import __description__
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name = "zerotasks",
+      version = __version__,
+      description = __description__,
+      long_description=read('README'),
+      license = __license__,
+      author = __author__,
+      author_email = __email__,
+      url = __url__,
+      packages = find_packages(),
+      keywords = ['realtime','data','local'],
+      zip_safe = True)
+
+
+# vim: ff=unix:ts=4:sw=4:tw=78:noai:expandtab
